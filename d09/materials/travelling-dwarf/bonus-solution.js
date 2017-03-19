ae84738a75f6930472c8ed5ed3d9857b2c8fb66e
ifbencode <bleong520@gmail.com>
2017-03-19 13:21:20 -0400

    Bonus tweak 1
    Got dwarf to move to next obstacle.
    
    Issues:
    Have to set variable to 1, bc 0 won’t work
    This results in formula problems in getting to 3rd obstacle as it seems
    off by 2?

--- d09/materials/travelling-dwarf/bonus-solution.js
+++ d09/materials/travelling-dwarf/bonus-solution.js
@@ -16,10 +16,22 @@ var coords = 0;
 
 var verBox = 1;
 
-while (g.at(0,verBox) === (0)) {
+while (g.at(4,verBox) === (0)) {
   d.move();
   coords = coords + 1;
   verBox = verBox + 1;
-} if (g.at(0, verBox) === "color grey") {
+} if (g.at(4, verBox) === "color grey") {
   d.orient("right");
+}
+
+var coords = 0;
+
+var horBox = 0;
+
+while (g.at(horBox,0) === (0)) {
+  d.move();
+  coords = coords + 1;
+  horBox = horBox + 1;
+} if (g.at(horBox, 0) === "color grey") {
+  d.orient("up");
 }
