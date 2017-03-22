b4dc726656611b78de9a09e71ca7ef488354eaba
ifbencode <bleong520@gmail.com>
2017-03-21 21:27:48 -0400

    Celsius project

--- d10/materials/temperature-converter/js/project.js
+++ d10/materials/temperature-converter/js/project.js
@@ -1,1 +1,9 @@
 console.log("celcius project");
+
+$("button").on("click", function() {
+  // console.log("pushed");
+  var degrees = $("input").val();
+  // console.log(degrees = degrees * 1.8 + 32);
+  degrees = degrees * 1.8 + 32;
+  $("#converted").text(degrees);
+})
