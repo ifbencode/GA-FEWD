324472b01976e8c4914a13519192fcbf6f61d0bb
ifbencode <bleong520@gmail.com>
2017-03-29 22:13:17 -0400

    Update to dropdown menu

--- d12/materials/dropdown-buttons/js/project.js
+++ d12/materials/dropdown-buttons/js/project.js
@@ -1,10 +1,32 @@
-console.log("Hi");
+// console.log("dropdown buttons")
 
-$("#dropdownOne").on("click", function() {
+$("#fileDropDown").on("click", function() {
     // console.log("clicked");
-    $("#dropdownMenu").toggleClass("show");
+    $("#fileDropMenu").toggleClass("show");
 })
 
-$("#dropdownOne").focus(function(){
-    $("#dropdownOne").css("background-color", "gray").fadeOut(2000);
-});
+$("#editDropDown").on("click", function() {
+    // console.log("clicked");
+    $("#editDropMenu").toggleClass("show");
+})
+
+// $("#fileDropDown").on("focusout", function() {
+//   console.log('focusout-check');
+//   // $("#dropdownOne").css("background-color", "white");
+// });
+
+//
+// $("#fileDropDown").mouseover(function() {
+// // console.log("hover check");
+//   $("#fileDropDown").css("background-color", "rgba(0, 0, 0, .1)");
+// })
+// $("#fileDropDown").mouseout(function() {
+//   $("#fileDropDown").css("background-color", "#FFFFFF");
+// })
+//
+// $("#fileDropMenu").mouseover(function() {
+//   $("#fileDropMenu").css("background-color", "rgba(0, 0, 0, .1)");
+// })
+// $("#fileDropMenu").mouseout(function() {
+//   $("#fileDropMenu").css("background-color", "#FFFFFF");
+// })
