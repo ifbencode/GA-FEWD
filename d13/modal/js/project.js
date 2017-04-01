fb3dcc961f423c0ac9151a2c3bc51652b9e66018
ifbencode <bleong520@gmail.com>
2017-04-01 17:55:45 -0400

    Added fadeToggle and change text on forms

--- d13/modal/js/project.js
+++ d13/modal/js/project.js
@@ -1,24 +1,35 @@
 
 $("#header-sign-in").on("click", function() {
   // console.log("clicked");
-  $("#form-container").toggleClass("show");
+  $("#form-container").fadeToggle(500);
   $("body").css("background-color", "rgba(0, 0, 0, .8)");
 })
 
+$("#input-email").on("click", function() {
+  console.log("input text");
+  $("#input-email").val(" ");
+})
+
+$("#input-pw").on("click", function() {
+  console.log("input text");
+  $("#input-pw").val(" ");
+  $("input-pw").val("password");
+})
+
+
 
 // $("body").on("focusout", function() {
-//   $("#form-container").toggleClass("show");
+//   $("#form-container").fadeToggle(500);
 //   $("body").css("background-color", "#FFFFFF");
 // })
 
-
-$('body').on("click", function(e) {
-  var target = e.target;
-  console.log($(target).attr('id'))
-
-  if ($(target).attr('id') !== 'form-container' && $(target).attr('id') !== 'header-sign-in') {
-    $("#form-container").removeClass("show");
-    $("body").css("background-color", "rgba(0, 0, 0, 0)");
-  }
-
-});
+//
+// $('body').on("click", function(e) {
+//   var target = e.target;
+//   console.log($(target).attr('id'))
+//
+//   if ($(target).attr('id') !== 'form-container' && $(target).attr('id') !== 'inputw') {
+//     $("#form-container").removeClass("show");
+//     $("body").css("background-color", "rgba(0, 0, 0, 0)");
+//   }
+// });
