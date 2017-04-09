69262af68eff5f92d4e1df660313f6feea1fc9b6
ifbencode <bleong520@gmail.com>
2017-04-09 14:40:34 -0400

    Added weather icon

--- d15/materials/weather/js/app.js
+++ d15/materials/weather/js/app.js
@@ -2,20 +2,22 @@
 // 2 Show icon
 // BONUS: Show forecast 24hs from now
 
-
 // $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Trenton,NJ&units=imperial&appid=b69d52d8b5e834e67958593dedc46fb6", function(data) {
-//   // console.log(data["main"]["temp"]);
+//   console.log(data["main"]["temp"]);
 // });
-
-
 
 $("button").on("click", function() {
   // console.log("pushed");
   var cityName = $("input").val();
-  // console.log(cityName);
+    // console.log(cityName);
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+(cityName)+"&units=imperial&appid=b69d52d8b5e834e67958593dedc46fb6", function(data) {
-    $("span").text(data["main"]["temp"]);
-    $("span").css("font-size", "3em");
-    $(".icon").html(data["weather"]["icon"]);
+    $(".temperature").text(data["main"]["temp"]);
+    $(".temperature").css("font-size", "3em");
+
+    var weatherIcon = data.weather[0].icon;
+    // console.log(weatherIcon);
+    var iconUrl = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+    // console.log(iconUrl);
+    $(".icon").attr("src", iconUrl);
   });
 })
